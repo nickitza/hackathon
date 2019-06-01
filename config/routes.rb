e53ce3920ebf3e6c7bf155ecf68201d52f9079f8
Nicki Nixon <nickitza@gmail.com>
2019-06-01 10:34:04 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root 'trips#index'
   
+  resources :trips do
+    resources :locations
+  end
+
+  resources :locations do
+    resources :addresses
+  end
+
 end
